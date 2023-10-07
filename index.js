142de01afa0f0c94ea7fd275bd1d62d3ab679197
hsanch13 <hayleysanchez13@gmail.com>
2023-10-07 14:59:39 -0600

    added functions

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Write your solution in this file!
+const employee = {
+    name: "Hayley",
+    streetAddress: "5762 Heathland Terrace",
+  };
+
+function updateEmployeeWithKeyAndValue(employeesObject, key, value) {
+    let newEmployees = {...employeesObject}; 
+    newEmployees [key] = value;
+    return newEmployees
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee [key] = value;
+    return employee
+
+}
+
+function deleteFromEmployeeByKey(employeeObject, key) {
+    let newestEmployee = {...employeeObject};
+    delete newestEmployee[key]
+    return newestEmployee
+}
+
+function destructivelyDeleteFromEmployeeByKey(employeeObject, key) {
+    delete employeeObject[key]
+    return employeeObject
+}
