f4cba3f4c59b4c3984cc64e663cc27c9e0db9e74
hsanch13 <hayleysanchez13@gmail.com>
2023-11-07 22:37:13 -0700

    made functions to modify object

--- index.js
+++ index.js
@@ -1,27 +1,58 @@
+//new code from phase 1:
+
 const employee = {
     name: "Hayley",
-    streetAddress: "5762 Heathland Terrace",
-  };
-
-function updateEmployeeWithKeyAndValue(employeesObject, key, value) {
-    let newEmployees = {...employeesObject}; 
-    newEmployees [key] = value;
-    return newEmployees
+    streetAddress: "100 Sundae Lane"
 }
 
-function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
-    employee [key] = value;
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    const newEmployeeObject = {...employee};
+    newEmployeeObject[key] = value;
+    return newEmployeeObject
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value){
+    obj[key] = value;
+    return obj
+}
+
+function deleteFromEmployeeByKey(obj, key){
+    const newestEmployeeObject = {...employee}
+    delete newestEmployeeObject.name
+    return newestEmployeeObject
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee.name
     return employee
-
 }
 
-function deleteFromEmployeeByKey(employeeObject, key) {
-    let newestEmployee = {...employeeObject};
-    delete newestEmployee[key]
-    return newestEmployee
-}
+// old code from pre-work:
 
-function destructivelyDeleteFromEmployeeByKey(employeeObject, key) {
-    delete employeeObject[key]
-    return employeeObject
-}
+//  const employee = {
+//     name: "Hayley",
+//     streetAddress: "57 Heathland Terrace",
+//   };
+
+// function updateEmployeeWithKeyAndValue(employeesObject, key, value) {
+//     let newEmployees = {...employeesObject}; 
+//     newEmployees [key] = value;
+//     return newEmployees
+// }
+
+// function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+//     employee [key] = value;
+//     return employee
+
+// }
+
+// function deleteFromEmployeeByKey(employeeObject, key) {
+//     let newestEmployee = {...employeeObject};
+//     delete newestEmployee[key]
+//     return newestEmployee
+// }
+
+// function destructivelyDeleteFromEmployeeByKey(employeeObject, key) {
+//     delete employeeObject[key]
+//     return employeeObject
+// }
